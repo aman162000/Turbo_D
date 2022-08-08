2c3514d92329eb14c4ee17c6fba228b78f48d3a4
aman162000 <borse.aman@rediffmail.com>
2022-08-08 10:33:12 +0530

    Update project name and add ico files

--- Turbo_C_Draw/Form1.Designer.cs
+++ Turbo_C_Draw/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Turbo_C_Draw
+﻿namespace Turbo_D
 {
     partial class Form1
     {
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.rectangle = new System.Windows.Forms.RadioButton();
@@ -449,6 +450,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Turbo D";
--- Turbo_C_Draw/Properties/Resources.Designer.cs
+++ Turbo_C_Draw/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Turbo_C_Draw.Properties {
+namespace Turbo_D.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Turbo_C_Draw.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Turbo_C_Draw.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Turbo_D.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
